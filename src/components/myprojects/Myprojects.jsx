58581b413498714e218742ae85e29264f303b532
Ajay Kushwaha <ajaykushwaha626162@gmail.com>
2024-04-22 08:44:05 +0530

    update github link

--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -43,7 +43,7 @@ const Myprojects = () => {
             <h3>My Portfolio(This website)</h3>
             <small className='text-light'>ReactJs</small>
             <div className="portfolio__item-cta">
-              <a href="https://github.com/satyam-kumar-21/portfolio" target="_blank" rel='noreferrer' className='btn'>Github</a>
+              <a href="https://github.com/ajaykushwaha0508/portfolio" target="_blank" rel='noreferrer' className='btn'>Github</a>
               
             </div>
             
